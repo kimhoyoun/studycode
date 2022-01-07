a52975e6da02fefb26d9b31cf01369f449f9955a
kimhoyoun <hoyoun1309@gmail.com>
2022-01-07 17:00:02 +0900

    0107 update

---
+++ src/day220107/org/guiex/Ex01Frame.java
@@ -0,0 +1,15 @@
+package day220107.org.guiex;
+
+import java.awt.Frame;
+
+public class Ex01Frame {
+
+	public static void main(String[] args) {
+		// 프레임 실행
+		// 단독으로 생성하기 / 상속을 받아서 생성하기
+		Frame frame = new Frame();
+		frame.setSize(640, 480);
+		frame.setVisible(true);
+	}
+
+}
